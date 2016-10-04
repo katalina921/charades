3690605d00b0387b7f628213c82f68dc7fed0bc9
katalina <katalinaarce34@gmail.com>
2016-10-04 13:20:30 -0700

    test

--- app/src/main/java/com/academy/app/charades/MainActivity.java
+++ app/src/main/java/com/academy/app/charades/MainActivity.java
@@ -7,7 +7,9 @@ import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +23,19 @@ public class MainActivity extends AppCompatActivity {
     private final SensorEventListener sensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
+            previousAcceleration = currentAcceleration;
+            currentAcceleration = (float)Math.sqrt(x * x + y * y + z * z);
+            float delta = currentAcceleration - previousAcceleration;
+            acceleration = acceleration * 0.9f + delta;
+
+            if(acceleration > 15){
+                Toast toast = Toast.makeText(getApplication(), "Device has shaken", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
 
         @Override
@@ -49,10 +63,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(sensorListener, accelermeter, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(sensorListener);
     }
 }
